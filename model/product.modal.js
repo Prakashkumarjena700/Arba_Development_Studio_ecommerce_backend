55832f7243654020c25de2075df3c4862d73dcee
Prakashkumarjena700 <prakashkumarjena367@gmail.com>
2024-04-20 00:21:27 +0530

    added slug in product

--- model/product.modal.js
+++ model/product.modal.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose')
 const productSchema = mongoose.Schema({
     title: { type: String, require: true },
     description: { type: String, require: true },
+    slug: { type: String, require: true },
     price: { type: Number, require: true },
     category: {
         type: mongoose.Schema.Types.ObjectId,
--- routes/product.routes.js
+++ routes/product.routes.js
@@ -55,8 +55,9 @@ productRoute.post("/create", async (req, res) => {
     try {
         const { title, description, price, category, image } = req.body
         let userId = req.userID
+        const slug = slugify(req.body.title, { lower: true, strict: true });
 
-        const product = new productModel({ title, description, price, category, image, owner: userId })
+        const product = new productModel({ title, description, price, slug, category, image, owner: userId })
         await product.save()
         res.send({ "msg": "Product has been created successfully", "success": true })
 
